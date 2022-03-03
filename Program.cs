a6b7b006e6cdeb3be0280b567cec2820a4da5d53
Emily Gray <87569476+emilypgray@users.noreply.github.com>
2022-03-03 19:28:55 +1100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1._6HD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            string line;
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"c:\test.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                System.Console.WriteLine(line);
+                counter++;
+            }
+
+            file.Close();
+            System.Console.WriteLine("There were {0} lines.", counter);
+            // Suspend the screen.  
+            System.Console.ReadLine();
+        }
+    }
+}
